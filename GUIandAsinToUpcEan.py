9b19317214dffa40123a3a4ce2a32e5e9a0c5aec
joychao0919 <65536323+joychao0919@users.noreply.github.com>
2021-12-04 16:44:29 -0500

    Update GUIandAsinToUpcEan.py

--- GUIandAsinToUpcEan.py
+++ GUIandAsinToUpcEan.py
@@ -7,6 +7,8 @@ Created on Wed Nov 24 18:22:35 2021
 """
 
 from tkinter import *
+import tkinter as tk
+
 from tkinter.scrolledtext import ScrolledText
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
@@ -14,99 +16,36 @@ import re
 import requests, json
 from requests.structures import CaseInsensitiveDict
 import http.client, urllib.request, urllib.parse, urllib.error, base64
+from ReviewDataCleaning import review_cleaning
+from SellersInfo import search
+from CheckAmazonReview import get_name
 
 class MainWindow:
-    def __init__(self, win):
+    def __init__(self, window):
         self.lbl = Label(window, text="Amazon URL here: ")
-        #self.lbl.place(x = 340, y = 20)
         self.lbl.pack(ipadx = 10, ipady = 10, fill = 'x')
         
         self.txtfld = Entry(window)
-        #self.txtfld.place(x = 300, y = 40)
         self.txtfld.pack(ipadx = 10, ipady = 10, fill = 'x')
         
         self.btn = Button(window, text = "Show Recommendations", command = self.checkValid)
-        #self.btn.place(x = 300, y = 80)
         self.btn.pack(ipadx = 10, ipady = 10, fill = 'x')
         
         self.message = Label(window, text = "")
-        #self.message.place(x = 340, y = 120)
         self.message.pack(ipadx = 10, ipady = 10, fill = 'x')
-        
-        self.recommends = ScrolledText(window, width = 800)
-        self.recommends.pack(ipadx = 10, ipady = 10)
-        self.recommends.insert(INSERT, """\
-        This is a scrolledtext widget to make tkinter text read only.
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        """)
-        self.recommends.configure(state = 'disabled')
-        
-        self.reviews = ScrolledText(window, width = 800)
-        self.reviews.pack(ipadx = 10, ipady = 10)
-        self.reviews.insert(INSERT, """\
-        This is a scrolledtext widget to make tkinter text read only.
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        Test
-        """)
-        self.reviews.configure(state = 'disabled')
                 
+        self.recommends = ScrolledText(
+            master = window, 
+            wrap = WORD,
+            height = 5
+        )
+        
+        self.reviews = ScrolledText(
+            master = window, 
+            wrap = WORD,
+        )
+                
+    # Check if input URL is valid
     def checkValid(self):
         url = self.txtfld.get()
         validate = URLValidator()
@@ -118,16 +57,18 @@ class MainWindow:
             print("Input url is not valid")
             self.message.config(text = "Input url is not valid")
     
+    # Get product ASIN
     def getAsin(self, url):
         asin = re.search(r'/[dg]p/([^/?]+)', url, flags=re.IGNORECASE)
         if asin:
             print(asin.group(1))
             myMessage = "ASIN: " + asin.group(1)
             self.message.config(text = myMessage)
-            self.getAPI(asin.group(1))
+            self.stringify(asin.group(1))
         else:
             self.message.config(text = "Input url is not valid")
-            
+    
+    # Get product UPC and EAN
     def getAPI(self, asin):
         url = "https://v3.synccentric.com/api/v3/products/search"
 
@@ -143,15 +84,38 @@ class MainWindow:
         upc = jsonResponse['data'][0]['attributes']['upc']
         ean = jsonResponse['data'][0]['attributes']['ean']
         
-        print("upc: " + upc)
-        print("ean: " + ean)
-        
         return upc, ean
 
-        #sIFe5XuTMF3k3gCYNx3iQvYd11r79oBmqkMUJ8Jba5qHocDYa61FAduCNYu0        
+    # Get review in string format for the product
+    def stringify(self, asin):
+        upc, ean = self.getAPI(asin)
+        print(upc)
+        print(ean)
+        name = get_name(asin)
 
-window = Tk()
+        sellers = ""
+        reviews = ""
+
+        for s in search(upc, name):
+            sellers += s + "\n\n"
+
+        for r in review_cleaning(asin):
+            reviews += r + "\n\n"
+        
+        self.recommends.delete('1.0', 'end')
+        self.recommends.insert('end', sellers)
+        self.recommends.pack(expand = Y, fill = BOTH)
+        self.recommends.update()
+        
+        self.reviews.configure(state = 'normal')
+        self.reviews.delete('1.0', 'end')
+        self.reviews.insert('end', reviews)
+        self.reviews.pack(expand = Y, fill = BOTH)
+        self.reviews.update()
+        self.reviews.configure(state = 'disabled')
+        
+window = tk.Tk()
 myWindow = MainWindow(window)
 window.title('Electronics Recommender')
-window.geometry("800x800")
+window.geometry("1200x800")
 window.mainloop()
